3818f7bb9379f8ed0b523b9435047ad943db91ed
Ben Garnaat <garnaat.ben@gmail.com>
2016-02-01 20:35:20 -0800

    portfolio basic layout

---
+++ js/object.js
@@ -0,0 +1,18 @@
+var data = [
+  {
+    title: 'title 1',
+    course: '201',
+    timeline: '4 days',
+    completed: '2016-01-24',
+    url: 'www.google.com',
+    description: 'this is text 1',
+  },
+  {
+    title: 'title 2',
+    course: '301',
+    timeline: '1 day',
+    completed: '2016-02-16',
+    url: 'www.google.com',
+    description: 'this is text 2',
+  }
+];
