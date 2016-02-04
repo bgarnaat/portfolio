f843c885d769f21d37e7ab615a6567e2ab608c63
Ben Garnaat <garnaat.ben@gmail.com>
2016-02-03 22:16:18 -0800

    class 03 portfolio.  revamped for mobile first

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var indexView = {};
 indexView.handleMainNav = function(event) {
   $('.nav_main').on('click', '.tab', function() {
     $('.mini_page').hide();
-    $('#' + $(this).data('content')).show();  // show only sections with an ID matching the tab 'content'
+    $('.' + $(this).data('content')).show();  // show only sections with a class matching the tab 'content'
   });
 
   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
@@ -11,6 +11,6 @@ indexView.handleMainNav = function(event) {
 
 $(document).ready(function() {
   indexView.handleMainNav();
-  $('#projects').hide();
-  $('#about').hide();
+  $('.projects').hide();
+  $('.about').hide();
 });
--- js/project_data.js
+++ js/project_data.js
@@ -1,26 +1,41 @@
 var data = [
   {
-    title: 'title 1',
+    title: 'Campfire Coffee',
     course: '201',
-    timeline: '4 days',
-    completed: '2016-01-24',
-    url: 'www.google.com',
-    description: 'this is text 1',
+    timeline: '16-20 hours (including pair programming)',
+    completed: '2016-01-15',
+    url: 'https://github.com/bgarnaat/campfire_coffee',
+    deployed: 'http://bgarnaat.github.io/campfire_coffee',
+    description: '<p>Campfire Coffee is a website designed for a fictional company as part of CodeFellows 201 curriculum using HTML, CSS, and JavaScript.</p>' +
+                 '<p>This website incorporates a landing page designed to remind users of camping using a series of images and colors requested by client.</p>' +
+                 '<p>Additionally, this website features a data page for tracking hourly sales at all locations with the ability to add additional locations</p>',
   },
   {
-    title: 'title 2',
-    course: '301',
-    timeline: '1 day',
-    completed: '2015-12-12',
-    url: 'www.google.com',
-    description: 'this is text 2',
+    title: 'BusMall',
+    course: '201',
+    timeline: 'approximately 12 hours (including pair programming)',
+    completed: '2016-01-22',
+    url: 'https://github.com/bgarnaat/busmall',
+    deployed: 'http://bgarnaat.github.io/busmall',
+    description: '<p>BusMall is a website designed for a fictional company as part of CodeFellows 201 curriculum using HTML, CSS, and JavaScript.</p>' +
+                 '<p>This website is designed for a company conducting research about product select in which members of a focus group are instructed to select an item they would be most likely to purchase from a set of three random images fifteen times.</p>' +
+                 '<p>Users are then given the option to see the results of their selections including times each image was clicked, times each image was displayed, and percent clicked of displayed.</p>' +
+                 '<p>This project also incorporates local storage to store the current state of the user survey so a user may exit and resume their survey at any point in time.</p>' +
+                 '<p>*** NOTE:  Local Storage must be cleared to reset the survey ***</p>',
   },
   {
-    title: 'title 3',
-    course: '401',
-    timeline: '0.5 days',
-    completed: '2016-01-02',
-    url: 'www.google.com',
-    description: 'this is text 3',
+    title: 'Mic Check',
+    course: '201',
+    timeline: '40+ hours',
+    completed: '2016-01-29',
+    url: 'https://github.com/bgarnaat/starving_artists',
+    deployed: 'http://bgarnaat.github.io/starving_artists',
+    description: '<p>Mic Check is a website designed as the final project as part of CodeFellows 201 curriculum using HTML, CSS, and JavaScript.  This website was designed as a collaborative effort within a group of four individuals with three main objectives.</p>' +
+                 '<p>The first and primary goal is to allow a user to search for shows that are playing by name, location (venue or neighborhood), date, and price range and display results in a card with all relavent information clearly visible.</p>' +
+                 '<p>The second objective was to allow users to enter their own show, play, concert, or performance into the website using the shows tab.</p>' +
+                 '<p>The page would addionally display the newly entered show in the same card fasion as displayed on the search page to inform then user of their entry.</p>' +
+                 '<p>The final goal of this project was to include a venues page to allow users to enter a new venue to the listing.</p>' +
+                 '<p>Additionally utilizes local storage across all pages to enable a user to select newly added venues for searching and adding shows, and shows for searching.</p>' +
+                 '<p>*** NOTE:  to clear local storage click on Sam\'s profile picture on the "About Us" page ***</p>',
   }
 ];
